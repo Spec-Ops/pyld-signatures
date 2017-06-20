5f24cca93d82be5b32b3a57a562e3443d8395707
Christopher Allan Webber <cwebber@dustycloud.org>
2017-06-20 11:47:30 -0500

    Stuff to facilitate hacking on this library from a Guix environment

---
+++ guix.scm
@@ -0,0 +1,36 @@
+(use-modules (ice-9 match)
+             (srfi srfi-1)
+             (guix gexp)
+             (guix packages)
+             (guix licenses)
+             (guix download)
+             (guix git-download)
+             (guix build-system python)
+             (gnu packages)
+             (gnu packages base)
+             (gnu packages python))
+
+(define %source-dir (dirname (current-filename)))
+
+(define python-pyld-signatures
+  (package
+    (name "python-pyld-signatures")
+    (version "git")
+    (source (local-file %source-dir
+                        #:recursive? #t
+                        #:select? (git-predicate %source-dir)))
+    (build-system python-build-system)
+    (propagated-inputs
+     `(("python-pyld" ,python-pyld)))
+    (home-page "https://github.com/Spec-Ops/pyld-signatures/")
+    (synopsis "Implementation of Linked Data Signatures using python-pyld.")
+    (description "python-pyld-signatures is a Python library implementing
+the Linked Data Signatures algorithm, for verifiable claims and other purposes.
+It builds on python-pyld and integrates nicely with existing json-ld applications.")
+    (license bsd-3)))
+
+;; Make sure we have Python in our environment
+(package
+  (inherit python-pyld-signatures)
+  (inputs
+   `(("python" ,python))))
